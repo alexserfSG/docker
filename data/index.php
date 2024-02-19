4e2ba6df44f223c6e8c3fbab3f72c05dca17eb41
aleksnoname <gitcommit@yandex.ru>
2024-02-19 13:54:41 +0300

    1

--- data/index.php
+++ data/index.php
@@ -1,5 +1,21 @@
 <?php
 declare(strict_types=1);
 
+$redis = new Redis();
+$redis->connect('redis');
 
-echo "It works!! bhnijnlk";
+$arr = [];
+
+if ($redis->ping()) {
+    $arr['redis'] = "Redis is work!";
+}
+
+
+$mc = new Memcached();
+$mc->addServer("memcached", 11211);
+
+$mc->set("memcached", "Memcached is work!");
+
+$arr['memcahed'] = $mc->get("memcached") ? $mc->get("memcached") : 'Memcached doesnt work ...';
+
+print_r($arr);
